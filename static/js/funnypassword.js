3821afd1fa0e7d9c2a8554d15e858c5267419f97
Franco Ramirez <francoramirezreyes@gmail.com>
2023-06-13 20:53:48 -0500

    Password mask improved

--- static/js/funnypassword.js
+++ static/js/funnypassword.js
@@ -4,19 +4,30 @@
   const maskField = document.getElementById("password-mask")
 
   // Handle password mask
-  const maskPassword = () => {
+  const maskPassword = length => {
     const minCharCode = 33
     const maxCharCode = 126
     let mask = ""
-    for (let i = 0; i < passwordField.value.length; i++) {
+    for (let i = 0; i < length; i++) {
       const charCode = Math.floor(Math.random() * (maxCharCode - minCharCode + 1) + minCharCode)
       mask += String.fromCharCode(charCode)
     }
     maskField.value = mask
   }
 
-  document.getElementById("login-password").addEventListener("keyup", maskPassword)
-  document.getElementById("login-password").addEventListener("keydown", maskPassword)
+  passwordField.addEventListener("keydown", e => {
+    // Only mutate for valid chars, backspace & delete
+    if (e.key.length > 1 && e.key !== "Backspace" && e.key !== "Delete") return
+    // Only allow backspace after the first letter, no selection
+    if (e.key === "Backspace" && e.target.selectionEnd === 0) return
+    // Only allow delete before the last letter, no selection
+    if (e.key === "Delete" && e.target.selectionStart === passwordField.value.length) return
+
+    // Update mask with delay
+    setTimeout(() => maskPassword(e.target.value.length), 1)
+  })
+
+  // passwordField.addEventListener("keypress", console.log)
 
   // Handle password visibility
   let passwordVisible = false
@@ -32,7 +43,7 @@
     visibilityStateButtons[passwordVisible].style.display = "none"
     visibilityStateButtons[!passwordVisible].style.display = "block"
     // Show/hide password
-    if (!passwordVisible) return maskPassword()
+    if (!passwordVisible) return maskPassword(passwordField.value.length)
     maskField.value = passwordField.value
     clearTimeout(currentTimeout)
     currentTimeout = setTimeout(() => handlePasswordVisibility(false), 5000)
